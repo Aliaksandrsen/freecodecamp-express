31b495d10d46f4399ef9e2e877597abed856c113
Glitch (scientific-neighborly-caravan) <none>
2020-07-19 15:44:37 +0000

    🛋🏖 Checkpoint ./myApp.js:19338754/448

--- myApp.js
+++ myApp.js
@@ -41,7 +41,10 @@ app.get('/json', (req, res, next) => {
 /** 6) Use the .env file to configure the app */
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  res.json(req.time)
+});
 
 /** 9)  Get input from client - Route parameters */
 
