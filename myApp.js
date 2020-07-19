2ee355e29806deca001f4074443bf1f017414a13
Glitch (scientific-neighborly-caravan) <none>
2020-07-19 17:14:39 +0000

    🚇🍋 Checkpoint ./myApp.js:19338754/365

--- myApp.js
+++ myApp.js
@@ -56,7 +56,11 @@ app.get('/:word/echo', (req, res, next) => {
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.get('/name', (req, res, next) => {
+  const first = req.query.first;
+  const last = req.query.last;
+  res.json({ name: `${first} ${last}`});
+});
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
